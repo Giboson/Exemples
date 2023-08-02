50607e5a18dfc784af5ad4fce6ffd849be98cc8e
Andrew <91674829+Giboson@users.noreply.github.com>
2023-08-02 23:36:56 +0500

    String

--- Example002_HiUserConsole/Program.cs
+++ Example002_HiUserConsole/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.WriteLine("Username");
-string usernmae = Console.ReadLine();
-Console.WriteLine("Hi, ");
-Console.WriteLine(usernmae); 
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Username");
+        string usernmae = Console.ReadLine();
+        Console.WriteLine("Hi, ");
+        Console.WriteLine(usernmae);
+    }
+}
