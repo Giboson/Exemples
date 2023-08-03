8c85b8f3f1a1bc1d0bc8d3b7eb20ccd34c00316c
Andrew <91674829+Giboson@users.noreply.github.com>
2023-08-03 08:48:18 +0500

    Nuber_8

--- Exemple003_Sun/Program.cs
+++ Exemple003_Sun/Program.cs
@@ -1,8 +1,12 @@
-﻿
+﻿// My class Example003_Sun
 internal class Program
 {
     public static void Main(string[] args)
     {
+        int numberA = 3;
+        int numberB = 5;
         
+        Console.WriteLine(numberA + numberB);
+
     }
 }
