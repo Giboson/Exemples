b5eb53ad3be2fe85ceadd6c23c00470b555754da
Andrew <91674829+Giboson@users.noreply.github.com>
2023-08-03 08:14:57 +0500

    Example003

--- Example001_helloConsole/Program.cs
+++ Example001_helloConsole/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("1 Hello, World!"); 
+    Console.WriteLine("2 Hello, World!");
+    }
+}
