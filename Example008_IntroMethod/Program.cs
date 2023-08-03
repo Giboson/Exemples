a8c2ba84eeb60d43c512128811993d373196b68a
Andrew <91674829+Giboson@users.noreply.github.com>
2023-08-03 22:16:17 +0500

    Example 008 max number_3

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -24,11 +24,9 @@
     int max3 = Max(a3,b3,c3);
 
     // if (b1 > max) max = b1; if (c1 > max) max = c1; if (a2 > max) max = a2; if (b2 > max) max = b2; if (c2 > max) max = c2; if (a3 > max) max = a3; if (b3 > max) max = b3; if (c3 > max) max = c3;
-    
-    
 
-    int max = Max (max1, max2, max3,); 
+    int max = Max(max1, max2, max3); 
     Console.WriteLine(max);
-    
+
     }
 }
