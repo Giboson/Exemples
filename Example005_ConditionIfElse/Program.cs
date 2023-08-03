b02c224ba8e4d6e20481937d74a08048350d0b40
Andrew <91674829+Giboson@users.noreply.github.com>
2023-08-03 16:34:51 +0500

    New Sendi

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -2,6 +2,18 @@
 {
     public static void Main(string[] args)
     {
- 
+        Console.Write("the username, ");
+        string username = Console.ReadLine();
+
+        if (username == "Sendi")
+        {
+            Console.WriteLine("You Sendi, ");
+        } 
+        else 
+        {
+            Console.Write("Hello, ");
+            Console.WriteLine(username);
+        }
+
     }
 }
