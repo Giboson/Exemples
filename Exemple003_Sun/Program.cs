eb9a9b68ec198d89da70f4b615c578899cc4bd40
Andrew <91674829+Giboson@users.noreply.github.com>
2023-08-03 11:33:15 +0500

    Number sum text Numbers

--- Exemple003_Sun/Program.cs
+++ Exemple003_Sun/Program.cs
@@ -4,7 +4,9 @@ internal class Program
     public static void Main(string[] args)
     {
         int numberA = new Random().Next(1, 10); // Numder Random 0 do 9 
-        int numberB = new Random().Next(1, 10); // Numder Random 0 do 9 
+        Console.WriteLine(numberA); 
+        int numberB = new Random().Next(1, 10); // Numder Random 0 do 9
+        Console.WriteLine(numberB); 
         // result
         int result = numberA + numberB;
         Console.WriteLine(result);
