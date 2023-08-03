244f2d87831725c5b7343e5d0b73279905481368
Andrew <91674829+Giboson@users.noreply.github.com>
2023-08-03 08:57:42 +0500

    add Example 003 sun

--- Exemple003_Sun/Program.cs
+++ Exemple003_Sun/Program.cs
@@ -3,10 +3,11 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        int numberA = 3;
+        int numberA = 31;
         int numberB = 5;
-        
-        Console.WriteLine(numberA + numberB);
+        // result
+        int result = numberA + numberB;
+        Console.WriteLine(result);
 
     }
 }
