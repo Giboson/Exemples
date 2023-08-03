a7dd982a7bccda111a8eab01c3e8abaa9be5bb41
Andrew <91674829+Giboson@users.noreply.github.com>
2023-08-03 10:14:24 +0500

    double div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        // int number =  10,  double = number 10.5 
+        double numberA = 12;
+        double numberB = 5;
+        Console.WriteLine(numberA / numberB);
+    }
+}
