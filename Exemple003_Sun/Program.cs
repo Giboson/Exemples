d8c057cc5e48870dbc077583a6d757acd1282d98
Andrew <91674829+Giboson@users.noreply.github.com>
2023-08-03 11:20:41 +0500

    random 1 = 10

--- Exemple003_Sun/Program.cs
+++ Exemple003_Sun/Program.cs
@@ -3,7 +3,7 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        int numberA = 31;
+        int numberA = new Random().Next(1, 10); // Numder Random 1 do 10 
         int numberB = 5;
         // result
         int result = numberA + numberB;
