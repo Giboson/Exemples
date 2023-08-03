6e4c1d183273e03a49c6ee2ca4e24d739f7c9ce0
Andrew <91674829+Giboson@users.noreply.github.com>
2023-08-03 22:24:02 +0500

    Example 008 max number_4

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -18,14 +18,15 @@
     int a3 = 13;
     int b3 = 23; 
     int c3 = 313;
-
-    int max1 = Max(a1,b1,c1);
-    int max2 = Max(a2,b2,c2);
-    int max3 = Max(a3,b3,c3);
-
-    // if (b1 > max) max = b1; if (c1 > max) max = c1; if (a2 > max) max = a2; if (b2 > max) max = b2; if (c2 > max) max = c2; if (a3 > max) max = a3; if (b3 > max) max = b3; if (c3 > max) max = c3;
-
-    int max = Max(max1, max2, max3); 
+    // code formating (1)(2)(3)
+    //(2) int max1 = Max(a1,b1,c1); int max2 = Max(a2,b2,c2); int max3 = Max(a3,b3,c3); int max = Max(max1, max2, max3); 
+    //(1) if (b1 > max) max = b1; if (c1 > max) max = c1; if (a2 > max) max = a2; if (b2 > max) max = b2; if (c2 > max) max = c2; if (a3 > max) max = a3; if (b3 > max) max = b3; if (c3 > max) max = c3;
+    
+    // (3) version
+    int max = Max(
+        Max(a1,b1,c1),
+        Max(a2,b2,c2),
+        Max(a3,b3,c3));
     Console.WriteLine(max);
 
     }
