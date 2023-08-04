f54bf25199e277a8d9ec26266768f55b353e0fbc
Andrew <91674829+Giboson@users.noreply.github.com>
2023-08-04 08:49:24 +0500

    Exemple 11 no error code xxx

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,7 +1,53 @@
-﻿internal class Program
-{
-    public static void Main(string[] args)
-    {
-       
-    }
-}
+﻿
+  
+        void FillArray(int[] collection)
+        {   
+            int length = collection.Length;
+            int index = 0;
+            int position = 0;
+            while (index < length)
+            {
+                collection[index] = new Random().Next(1, 10);
+                // index = index + 1;
+                index++;
+            }
+
+        }
+        void PrintArray(int[] coll)
+        {
+            int  count = coll.Length;
+            int position = 0;
+
+            while(position < count)
+            {
+                Console.WriteLine(coll[position]);
+                position++;
+            }
+        }
+        int IndexOf(int[] collection, int find);
+        
+        int count = Collection.Length;
+        index = 0;
+        index position = 0;
+        while(index < count)
+        {
+            int count = collection.Length;
+            if (collection[index] == find)
+            {
+                position = index;
+                break;
+
+            }
+            index++;
+        } 
+        return position;
+    //                      { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+       int[] array = new int[10]; 
+    
+    FillArray(array);
+    PrintArray(array);
+    Console.WriteLine(); 
+
+    int pos = IndexOf(array, 4);
+    Console.WriteLine(pos);
+
