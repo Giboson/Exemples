24c72b9f8c3ac7e979c9e31c247adc30ecc173d4
Andrew <andrewyarygin534@gmail.com>
2023-08-13 01:26:40 +0500

    book info code c#

--- book/simpleshrpadd/Program.cs
+++ book/simpleshrpadd/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿            // Set up Console UI (CUI)
+            Console.Title = "My Rocking App";
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("***********************************");
+            Console.WriteLine("*****Welcome to My Rocking App*****");
+            Console.WriteLine("***********************************");
+            Console.BackgroundColor = ConsoleColor.Black;
+            //  Wait for button  <Enter> kay to be Pressed
+             Console.ReadLine();
+/**
+            Console.WriteLine("**************************");
+            Console.WriteLine("***** My First C# App*****");
+            Console.WriteLine("**************************");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("args: {0}", args[i]);
+            }
+             //  Wait for button  <Enter> kay to be Pressed
+             Console.ReadLine();
+            return 0;
+**/
